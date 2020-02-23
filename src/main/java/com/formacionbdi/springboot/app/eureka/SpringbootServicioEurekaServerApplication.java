099fedd932c700cce467b74db2edab0da9dc85a1
MAALONSO <miguel.coba@gnp.com.mx>
2020-02-22 20:23:01 -0600

    Agregando la configuracion para usar el servidor de Eureka

--- src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -3,7 +3,9 @@ package com.formacionbdi.springboot.app.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * Se habilita EurekaServier con la anotacion @EnableEurekaServer.
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class SpringbootServicioEurekaServerApplication {
